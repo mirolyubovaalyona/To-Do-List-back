61ecc03871bc207a0bbe73e0ee2c0d74aa4287cf
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-10-26 02:28:45 +1000

    create Tasks table

---
+++ database/migrations/2024_10_25_162711_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedTinyInteger('priority')->default(0);
+            $table->enum('type', ['due_date', 'date_range', 'weekly', 'daily'])->default('due_date');
+            $table->date('due_date')->nullable(); 
+            $table->date('start_date')->nullable(); 
+            $table->date('end_date')->nullable(); 
+            $table->json('days_of_week')->nullable(); 
+            $table->date('daily')->nullable(); 
+            $table->timeTz('time')->nullable(); 
+            $table->boolean('is_completed')->default(0);;
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
